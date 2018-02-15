12e4bf4d53cef4f9ea577c0abc3eb55e8ead3f93
janani krishna <jkrishna@umass.edu>
2018-02-15 16:36:18 -0500

    fixed the exception
    issue : number of positive and negative is too much

--- preprocess.py
+++ preprocess.py
@@ -4,25 +4,32 @@ from rdkit.Chem.rdMolDescriptors import _CalcMolWt
 countp = 0
 countn = 0
 count = 0
-with open('BindingDB_All.tsv') as tsvfile:
+with open('../BindingDB_All.tsv') as tsvfile:
 	reader = csv.DictReader(tsvfile, dialect='excel-tab')
 	for row in reader:
+		count = count +1
+		# import pdb
+		# pdb.set_trace()
 		try:
-			if(row['IC50 (nM)'] == ''):
+			if(row['IC50 (nM)'] == '' or row['Ligand SMILES']=='' or row['Ligand SMILES'] is None):
 				continue
-			elif float(row['IC50 (nM)'])<100 and _CalcMolWt(Chem.MolFromSmiles(row['Ligand SMILES']))<1000:
-				countp = countp+1
-			elif float(row['IC50 (nM)'])>1000 and _CalcMolWt(Chem.MolFromSmiles(row['Ligand SMILES']))<1000:
-				countn = countn+1
+			else:
+				if float(row['IC50 (nM)'])<100 and _CalcMolWt(Chem.MolFromSmiles(row['Ligand SMILES']))<1000:
+					countp = countp+1
+				elif float(row['IC50 (nM)'])>1000 and _CalcMolWt(Chem.MolFromSmiles(row['Ligand SMILES']))<1000:
+					countn = countn+1
 		except ValueError:
-			# str = row['IC50 (nM)']
-			str = row['IC50 (nM)']
-			if str[0]=='>' and float(str[1:])>1000 and _CalcMolWt(Chem.MolFromSmiles(row['Ligand SMILES']))<1000:
-				countn = countn+1
-			elif str[0]=='<' and float(str[1:])<100 and _CalcMolWt(Chem.MolFromSmiles(row['Ligand SMILES']))<1000:
-				countp = countp+1
-
+			try:
+				str = row['IC50 (nM)']
+				if str[0]=='>' and float(str[1:])>1000 and _CalcMolWt(Chem.MolFromSmiles(row['Ligand SMILES']))<1000:
+					countn = countn+1
+				elif str[0]=='<' and float(str[1:])<100 and _CalcMolWt(Chem.MolFromSmiles(row['Ligand SMILES']))<1000:
+					countp = countp+1
+			except TypeError:
+				continue
 		except TypeError:
+			continue
+		except:
 			continue
 print "no of positives",countp
 print "no of negatives",countn
