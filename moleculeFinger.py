961af5fbc5a8faa62b9b4f33ad8d1c5250c62724
janani krishna <jkrishna@umass.edu>
2018-02-22 12:40:02 -0500

    run molecularFinger to get the feature vector
    change the path accordingly to the dataset

--- moleculeFinger.py
+++ moleculeFinger.py
@@ -3,6 +3,7 @@ from rdkit import Chem, DataStructs
 from rdkit.Chem.rdMolDescriptors import _CalcMolWt
 from rdkit.Chem import AllChem
 import numpy
+from read_fasta import calPscIdx
 
 
 def convToArr(fp):
@@ -14,51 +15,61 @@ count = 0
 countp  = 0
 countn = 0
 pubids = list()
+psc_array,prot_id2idx = calPscIdx()
+
+
 with open('../BindingDB_All.tsv') as tsvfile,open('../newData.csv','w') as newfile:
-	reader = csv.DictReader(tsvfile,dialect='excel-tab')
+	next(tsvfile)
+	reader = csv.reader(tsvfile,delimiter='\t')
 	writer = csv.writer(newfile,delimiter=',')
-	writer.writerow(["PubChem CID", "fingerprintR1", "fingerprintR2","fingerprintR3","protienId"])
+	writer.writerow(["PubChem CID","protienId", "fingerprintR1", "fingerprintR2","fingerprintR3","protein descriptor","label"])
 	for row in reader:
-		pid = row['PubChem CID']
-		proId = row['UniProt (SwissProt) Primary ID of Target Chain']
-		if pid!='' and pid not in pubids:
+		# print len(row)
+		pid = row[28]
+		# import pdb
+		# pdb.set_trace()
+	
+		if pid!='' and pid not in pubids and row[41]!='':
 			pubids.append(pid)
+			# import pdb
+			# pdb.set_trace()
+			pd = psc_array[prot_id2idx[row[41].split(',')[0]]]
 			try:
-				if(row['IC50 (nM)']=='' or row['Ligand SMILES']=='' or row['Ligand SMILES'] is None):
+				if(row[9]=='' or row[1]=='' or row[1] is None):
 					continue
 				else:
-					md = Chem.MolFromSmiles(row['Ligand SMILES'])
-					if float(row['IC50 (nM)'])<100 and _CalcMolWt(md)<1000 and md is not None:
+					md = Chem.MolFromSmiles(row[1])
+					if float(row[9])<100 and _CalcMolWt(md)<1000 and md is not None:
 						fp1 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,1))
 						fp2 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,2))
 						fp3 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,3))
-						out = pid,fp1.tolist(),fp2.tolist(),fp3.tolist(),proId
+						out = pid,proId,fp1.tolist(),fp2.tolist(),fp3.tolist(),pd,1
 						writer.writerow(out)
 						countp += 1
-					elif float(row['IC50 (nM)'])>1000 and _CalcMolWt(md)<100 and md is not None:
+					elif float(row[9])>1000 and _CalcMolWt(md)<100 and md is not None:
 						fp1 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,1))
 						fp2 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,2))
 						fp3 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,3))
-						out = pid,fp1.tolist(),fp2.tolist(),fp3.tolist(),proId
+						out = pid,proId,fp1.tolist(),fp2.tolist(),fp3.tolist(),pd,0
 						writer.writerow(out)						
 						countn += 1
 			except ValueError:
 				try:
-					str = row['IC50 (nM)']
-					md = Chem.MolFromSmiles(row['Ligand SMILES'])
+					str = row[9]
+					md = Chem.MolFromSmiles(row[1])
 					if str[0]=='>' and float(str[1:])>1000 and _CalcMolWt(md)<1000 and md is not None:
 						countn += 1
 						fp1 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,1))
 						fp2 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,2))
 						fp3 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,3))
-						out = pid,fp1.tolist(),fp2.tolist(),fp3.tolist(),proId
+						out = pid,proId,fp1.tolist(),fp2.tolist(),fp3.tolist(),pd,0
 						writer.writerow(out)
 					elif str[0]=='<' and float(str[1:])<100 and _CalcMolWt(md)<1000 and md is not None:
 						countp += 1
 						fp1 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,1))
 						fp2 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,2))
 						fp3 = convToArr(AllChem.GetMorganFingerprintAsBitVect(md,3))
-						out = pid,fp1.tolist(),fp2.tolist(),fp3.tolist(),proId
+						out = pid,proId,fp1.tolist(),fp2.tolist(),fp3.tolist(),pd,1
 						writer.writerow(out)
 				except TypeError:
 					continue
--- read_fasta.py
+++ read_fasta.py
@@ -1,40 +1,43 @@
 import csv
 import numpy as np
 from calculate_psc import calculate_psc
-
-with open('BindingDB_All.tsv') as tsvfile:   
-    reader=csv.reader(tsvfile, delimiter='\t')
-    count=0
-    greater_49=0
-    less_49=0
-    c=0 
-    dict_prot={}
-    for row in reader:
-        
-       # print(row)
-        if(len(row)>49 and count!=1):
-            greater_49+=1
-            continue
+def calPscIdx():
+    with open('../BindingDB_All.tsv') as tsvfile:   
+        reader=csv.reader(tsvfile, delimiter='\t')
+        count=0
+        greater_49=0
+        less_49=0
+        c=0 
+        dict_prot={}
+        for row in reader:
             
-        if(len(row)<49):
-            less_49+=1
-            continue
-            
-        if(row[41]!=''):
-            #row[37]=
-            dict_prot[row[41].split(',')[0]]=row[37]
-      #  print row[41]
-        count+=1
+           # print(row)
+            if(len(row)>49 and count!=1):
+                greater_49+=1
+                continue
+                
+            if(len(row)<49):
+                less_49+=1
+                continue
+                
+            if(row[41]!=''):
+                #row[37]=
+                dict_prot[row[41].split(',')[0]]=row[37]
+          #  print row[41]
+            count+=1
 
-psc_array = np.zeros([len(dict_prot),8420])
-j=0
-#foo=0
-for i in dict_prot.keys():
- #   foo+=1
-  #  print dict_prot[i]
-    psc_array[j]=calculate_psc(dict_prot[i])
-    
-    j+=1
+    psc_array = np.zeros([len(dict_prot),8420])
+    j=0
+    prot_id2idx = {}
+    #foo=0
+    for i in dict_prot.keys():
+     #   foo+=1
+      #  print dict_prot[i]
+        psc_array[j]=calculate_psc(dict_prot[i])
+        prot_id2idx[i] = j
+        j+=1
+
+    return psc_array,prot_id2idx
     #p.savetxt("foo.csv", g, delimiter=",")
   #  fd.write(g)
     #print g[0]
